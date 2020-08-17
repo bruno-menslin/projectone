83c95864b1d4b07230648ee8918ba6c535808975
bruno-menslin <bmenslin@gmail.com>
2020-08-17 19:29:05 -0300

    versao jean 17/08

--- security/authentication/logout.php
+++ security/authentication/logout.php
@@ -1,7 +1,5 @@
 <?php
-    include "../../security/authentication/validation.php";
-
-    // session_start();
+    session_start();
     session_unset(); //'limpar' as variáveis
     session_destroy(); //destruir
     header("Location: ../../index.php");
