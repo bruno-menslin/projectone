82f76348118c019c28ae18037f198bd7a2389676
bruno-menslin <bmenslin@gmail.com>
2020-08-24 14:32:49 -0300

    UPD: login.php - carregamento centralizado de mensagens

--- security/authentication/login.php
+++ security/authentication/login.php
@@ -3,15 +3,17 @@
     $senha = $_POST['senha'];
 
     $msg = '';
+    $link = "../../index.php?mensagem="; // colocar a $msg aqui pra nao ir uma msg vazia pro main.php
 
     if ($usuario == '') {
         $msg = "Preencha o campo usuário.";
     } else if ($senha == '') {
         $msg = "Preencha o campo senha.";
     } else {
-        include "../database/connection.php";
 
         $senha = md5($senha);
+
+        include "../database/connection.php";
 
         $sql = "SELECT usuario, senha FROM usuarios WHERE usuario = :usuario AND senha = :senha";
         
@@ -21,21 +23,16 @@
         $stm_sql -> execute();
 
         if ($stm_sql -> rowCount() == 1) {
-            // header("Location: ../../app/main.php");
             session_start();
+
             $_SESSION['usuario'] = $usuario;
-            $_SESSION['senha'] = $senha; // todas as senhas em texto claro para criptografada
+            $_SESSION['senha'] = $senha;
             $_SESSION['idsessao'] = session_id();
 
-            header("Location: ../../app/main.php");
+            $link = "../../app/main.php";
         } else {
             $msg = "Usuário ou senha incorretos.";
         }
     }
+    header("Location: " . $link . $msg); // nunca usar mais de um header na mesma página
 ?>
-
-<h1>Aviso!</h1>
-<p>
-    <?php echo $msg; ?>
-</p>
-<a href="../../index.php">Voltar</a>
