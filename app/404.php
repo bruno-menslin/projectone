aeee70dfcd4330f278dfb069b1d770e70a0b392a
bruno-menslin <bmenslin@gmail.com>
2020-08-31 15:30:52 -0300

    UPD: 404.php - aplicacao bootstrap

--- app/404.php
+++ app/404.php
@@ -1,2 +1,4 @@
-<h1>404</h1>
-<h3>Página <strong><?php echo $_GET['folder'] . $_GET['file']; ?></strong> não encontrada.</h3>
+<div class="col-md-12 mt-3">
+    <h1><strong>404</strong></h1>
+    <h3>Página <strong><?php echo $_GET['folder'] . $_GET['file']; ?></strong> não encontrada.</h3>
+</div>
