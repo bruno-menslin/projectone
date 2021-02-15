84e753ee03ec077da44883c87d46f9ac252079bb
Bruno Menslin <brunomenslin@outlook.com>
2021-02-15 13:31:59 -0300

    style: update page logo

--- app/main.php
+++ app/main.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Bruno Menslin">
-        <title>Project One - Menu</title>
+        <title>PROJECT ONE</title>
         <link rel="stylesheet" href="../assets/css/bootstrap.css">
         <link rel="stylesheet" href="../assets/css/main.css">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -17,7 +17,7 @@
     <body>
         <header>            
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="main.php">Project One</a>
+                <a id="navbar-logo" class="navbar-brand" href="main.php">PROJECT ONE</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
                     <span class="navbar-toggler-icon"></span>
                 </button>
